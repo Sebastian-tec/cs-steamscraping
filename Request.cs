e589c45a8a3d7f72cc4612472c13f7bb62ad2823
S <111432890+Sebastian-tec@users.noreply.github.com>
2023-10-02 20:36:27 +0200

    Change name of method

--- Request.cs
+++ Request.cs
@@ -1,16 +1,10 @@
 ﻿using HtmlAgilityPack;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace cs_steamscraping
 {
     internal class Request
     {
-        public async ValueTask<HtmlDocument> GetWebRequest(string url)
+        public async ValueTask<HtmlDocument> GetDocument(string url)
         {
             return await new HtmlWeb().LoadFromWebAsync(url);
         }
